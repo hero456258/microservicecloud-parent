bec1498866fa4744da0a351cf1c3242da2476165
wanggang <wanggang@euka.cn>
2019-03-24 21:37:58 +0800

    eureka集群配置

---
+++ microservicecloud-eureka-7002/src/main/java/com/mingqian/springcloud/MicroservicecloudEureka7002Application.java
@@ -0,0 +1,15 @@
+package com.mingqian.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer   //EurekaServer服务启动类，接受其他微服务注册进来
+public class MicroservicecloudEureka7002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicecloudEureka7002Application.class, args);
+	}
+
+}
