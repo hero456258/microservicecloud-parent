d5d195e91e9b6367b862d13597750a0c777e907b
wanggang <wanggang@euka.cn>
2019-03-24 17:51:34 +0800

    将已有的8001服务提供者入驻到注册中心Eureka

--- microservicecloud-provider-dept-8001/src/main/java/com/mingqian/springcloud/MicroservicecloudProviderDept8001Application.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/mingqian/springcloud/MicroservicecloudProviderDept8001Application.java
@@ -2,8 +2,10 @@ package com.mingqian.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
 public class MicroservicecloudProviderDept8001Application {
 
 	public static void main(String[] args) {
