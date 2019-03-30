49f2e295fa773fa2f692f6528edda96db4801df9
wanggang <wanggang@euka.cn>
2019-03-30 08:19:54 +0800

    Ribbon初步配置

--- microservicecloud-consumer-dept-80/src/main/java/com/mingqian/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/mingqian/springcloud/cfgbeans/ConfigBean.java
@@ -1,5 +1,6 @@
 package com.mingqian.springcloud.cfgbeans;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,7 +10,9 @@ import org.springframework.web.client.RestTemplate;
  */
 @Configuration
 public class ConfigBean {
+
     @Bean
+    @LoadBalanced   //Spring Cloud  Ribbon是基于Netflix实现的一套客户端 负载均衡的工具
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
--- microservicecloud-consumer-dept-80/src/main/java/com/mingqian/springcloud/controller/deptConsumer/DeptConsumerController.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/mingqian/springcloud/controller/deptConsumer/DeptConsumerController.java
@@ -9,14 +9,14 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
-/**
- * Created by wanggang on 2019/3/24.
- */
 @RestController
 @RequestMapping("/consumer")
 public class DeptConsumerController {
 
-    private static final String REST_URL_PREFIX = "http://localhost:8001";
+    //private static final String REST_URL_PREFIX = "http://localhost:8001";
+    //修改成微服务的名称
+    private static final String REST_URL_PREFIX = "http://MICROSERVICECLOUD-DEPT";
+
 
     /**
      * 使用 使用restTemplate访问restful接口非常的简单粗暴无脑。 (url, requestMap,
@@ -26,26 +26,27 @@ public class DeptConsumerController {
     private RestTemplate restTemplate;
 
     @RequestMapping("/dept/add")
-    public boolean add(Dept dept){
+    public boolean add(Dept dept) {
         return restTemplate.postForObject(REST_URL_PREFIX + "/dept/add", dept, Boolean.class);
     }
 
     @RequestMapping("/dept/get/{id}")
-    public Dept get(@PathVariable("id") Long id){
-        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/get/" + id,Dept.class);
+    public Dept get(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/get/" + id, Dept.class);
     }
 
     @RequestMapping("/dept/list")
-    public List<Dept> list(){
-        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/list",List.class);
+    public List<Dept> list() {
+        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/list", List.class);
     }
 
     /**
      * 消费端可以调用服务发现
+     *
      * @return
      */
     @RequestMapping(value = "/dept/discovery")
-    public Object discovery(){
+    public Object discovery() {
         return restTemplate.getForObject(REST_URL_PREFIX + "/dept/discovery", Object.class);
     }
 }
