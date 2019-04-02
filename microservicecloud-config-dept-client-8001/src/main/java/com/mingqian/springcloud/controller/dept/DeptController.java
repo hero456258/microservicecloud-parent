d3dce5542e09cce37b27194768b4ca13d34382da
wanggang <wanggang@euka.cn>
2019-04-02 21:05:14 +0800

    SpringCloud Config配置实战

---
+++ microservicecloud-config-dept-client-8001/src/main/java/com/mingqian/springcloud/controller/dept/DeptController.java
@@ -0,0 +1,53 @@
+package com.mingqian.springcloud.controller.dept;
+
+import com.mingqian.springcloud.entity.Dept;
+import com.mingqian.springcloud.service.dept.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by wanggang on 2019/3/24.
+ */
+@RestController
+@RequestMapping("/dept")
+public class DeptController {
+
+    @Autowired
+    private DeptService deptService;
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/add",method = RequestMethod.POST)
+    public boolean add(@RequestBody Dept dept){
+        return deptService.add(dept);
+    }
+
+    @RequestMapping(value = "/get/{id}",method = RequestMethod.GET)
+    public Dept get(@PathVariable("id") Long id){
+        return deptService.get(id);
+    }
+
+    @RequestMapping(value = "/list",method = RequestMethod.GET)
+    public List<Dept> list(){
+        return deptService.list();
+    }
+
+    @RequestMapping(value = "/discovery", method = RequestMethod.GET)
+    public Object discovery()
+    {
+        List<String> list = client.getServices();
+        System.out.println("**********" + list);
+
+        List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
+        for (ServiceInstance element : srvList) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                    + element.getUri());
+        }
+        return this.client;
+    }
+}
